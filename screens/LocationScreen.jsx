c0f94f0e214fb449ed5f3b27ce4c8be7d51d36d3
Lee <leejavaa@gmail.com>
2023-01-03 06:30:13 +0200

    BUZ-25-Asset Caching Complete

--- screens/LocationScreen.jsx
+++ screens/LocationScreen.jsx
@@ -1,10 +1,25 @@
 import { View, Text } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import LocationCard from "../components/LocationCard";
 import firestore from "@react-native-firebase/firestore";
+import { Asset } from "expo-asset";
+import * as SplashScreen from "expo-splash-screen";
+
+function cacheImages(images) {
+  return images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+SplashScreen.preventAutoHideAsync();
 
 const LocationScreen = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [locations, setLocations] = useState([]);
 
   function onResult(QuerySnapshot) {
@@ -17,10 +32,49 @@ const LocationScreen = () => {
 
   useEffect(() => {
     firestore().collection("Locations").onSnapshot(onResult, onError);
+
+    async function loadResourcesAndDataAsync() {
+      try {
+        const imageAssets = cacheImages([
+          require("../assets/media/locations/cape-town.png"),
+          require("../assets/media/locations/durban.png"),
+          require("../assets/media/locations/stellenbosch.png"),
+          require("../assets/media/locations/joburg.png"),
+          require("../assets/media/locations/stellenbosch.png"),
+          require("../assets/media/clubs/aandklas.webp"),
+          require("../assets/media/clubs/de_lapa.png"),
+          require("../assets/media/clubs/dorp.png"),
+          require("../assets/media/clubs/fools_gold.png"),
+        ]);
+
+        await Promise.all([...imageAssets]);
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    loadResourcesAndDataAsync();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <SafeAreaView className="bg-veryDark flex-1">
+    <SafeAreaView className="bg-veryDark flex-1" onLayout={onLayoutRootView}>
       <View className=" px-10">
         <Text className="text-light font-semibold text-3xl my-5">
           Where we heading?
