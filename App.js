c3658aad0e3b1ec26d53174b31e4f718021d340e
Lee <leejavaa@gmail.com>
2023-02-13 08:25:53 +0200

    Logging screen views

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigationContainerRef } from "@react-navigation/native";
+import { useRef } from "react";
+import analytics from "@react-native-firebase/analytics"
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
+  const routeNameRef = useRef();
+  const navigationRef = useNavigationContainerRef();
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        routeNameRef.current = navigationRef.current.getCurrentRoute().name;
+      }}
+      onStateChange={async () => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+        routeNameRef.current = currentRouteName;
+        // console.log(routeNameRef.current)
+      }}
+    >
       <StackNavigator />
     </NavigationContainer>
   );
